6c11176f49b66ea42bca9e91951a3402727c4e29
jinyingjie <1406060330@qq.com>
2019-12-19 17:40:32 +0800

    主页右侧头部完成

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-row class="layout-header" type="flex" align="middle" justify="space-between">
+      <!-- 左侧内容 -->
+      <el-col class="left" :span="8">
+          <i class="el-icon-s-fold"></i>
+          <span class="title">江苏传智播客教育科技股份有限公司</span>
+      </el-col>
+      <!-- 右侧内容 -->
+      <el-col class="right" :span="4">
+          <el-row type="flex" justify="end">
+              <img src="../../assets/header.jpg" alt="">
+          <!-- 下拉菜单 -->
+          <el-dropdown class="right-text">
+              <span>靳小靳</span>
+              <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>个人信息</el-dropdown-item>
+                  <el-dropdown-item>GIT地址</el-dropdown-item>
+                  <el-dropdown-item>退出</el-dropdown-item>
+              </el-dropdown-menu>
+          </el-dropdown>
+          </el-row>
+
+      </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .layout-header {
+       height: 60px;
+        .left {
+             font-size: 18px;
+             .title {
+                 margin-left: 5px;
+                 font-size: 16px;
+                 color: #2c3e50;
+             }
+        }
+        .right {
+            img {
+                width: 50px;
+                height: 50px;
+                border-radius: 50%;
+                margin-right: 15px;
+            }
+            .right-text {
+                span {
+                    line-height: 50px;
+                }
+            }
+        }
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,29 @@
 <template>
   <el-container>
     <!-- 先左右布局 -->
-    <el-aside>左侧</el-aside>
-    <!-- 右侧 -->
+    <el-aside style="background-color:#353b4e;min-height:100vh;width:230px">
+      <layout-aside></layout-aside>
+    </el-aside>
+    <!-- 右侧布局 -->
     <el-container>
-      <el-header>头部</el-header>
+      <el-header>
+        <layout-header></layout-header>
+      </el-header>
       <el-main>主要区域</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {}
+import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
+export default {
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+
+  }
+}
 </script>
 
 <style>
