c13512b29eb05f6b724fd4a747b6680ed98553da
jinyingjie <1406060330@qq.com>
2019-12-19 16:18:19 +0800

    主页底层布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="home">
-      <span>主页</span>
-  </div>
+  <el-container>
+    <!-- 先左右布局 -->
+    <el-aside>左侧</el-aside>
+    <!-- 右侧 -->
+    <el-container>
+      <el-header>头部</el-header>
+      <el-main>主要区域</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
