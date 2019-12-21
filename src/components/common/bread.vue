e8d33911f3d8f164c4757f095dc1bc1cc75a1cff
jinyingjie <1406060330@qq.com>
2019-12-21 17:28:57 +0800

    面包屑组件封装

---
+++ src/components/common/bread.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 面包屑内容 -->
+    <el-breadcrumb>
+         <el-breadcrumb-item >
+             <slot name="title"></slot>
+         </el-breadcrumb-item>
+    </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
+// 统一管理组件
 import LayoutAside from './home/layout-aside'
 import LayoutHeader from './home/layout-header'
+import Bread from './common/bread'
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside)
     Vue.component('layout-header', LayoutHeader)
+    Vue.component('bread-crumb', Bread)
   }
 }
