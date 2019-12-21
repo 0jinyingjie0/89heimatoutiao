2fd7166a9a3583b95473ba27821aa561487cddbf
jinyingjie <1406060330@qq.com>
2019-12-21 16:28:56 +0800

    axios拦截器-统一处理响应

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,4 +8,12 @@ axios.interceptors.request.use(function (config) {
   // 执行请求失败
   console.log(error)
 })
+// 响应式拦截
+axios.interceptors.response.use(function (response) {
+  // 解决当data不存在时，报错的情况
+  return response.data ? response.data : {}
+}, function (error) {
+  // 执行请求失败
+  console.log(error)
+})
 export default axios
