cbe45266d037c193d7b934ce6dd466d77ec0ccea
jinyingjie <1406060330@qq.com>
2019-12-21 17:53:30 +0800

    评论列表-页面结构

--- src/components/common/bread.vue
+++ src/components/common/bread.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- 面包屑内容 -->
-    <el-breadcrumb>
+    <el-breadcrumb separator=">">
+        <el-breadcrumb-item>首页</el-breadcrumb-item>
          <el-breadcrumb-item >
              <slot name="title"></slot>
          </el-breadcrumb-item>
