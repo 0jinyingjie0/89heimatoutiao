1b0fa862db07f0788f398de0277a52cf61535bc3
jinyingjie <1406060330@qq.com>
2019-12-24 19:52:21 +0800

    内容列表

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -43,6 +43,31 @@
         ></el-date-picker>
       </el-col>
     </el-row>
+    <!-- 主体 -->
+    <el-row class="total">
+      <span>共找到10条内容</span>
+    </el-row>
+    <el-row v-for="item in 10" :key="item" type="flex" justify="space-between" class="item-ar">
+      <el-col :span="6">
+        <el-row type="flex">
+          <img src="../../assets/header.jpg" alt />
+          <div class="info">
+            <!-- 标题 -->
+            <span>1</span>
+            <!-- 种类标签 -->
+            <el-tag class="tag">标签一</el-tag>
+            <!-- 时间 -->
+            <span class="date">2</span>
+          </div>
+        </el-row>
+      </el-col>
+      <el-col :span="6">
+        <el-row type="flex" justify="end" class="right">
+          <span><i class="el-icon-edit"></i>修改</span>
+          <span><i class="el-icon-delete"></i>删除</span>
+        </el-row>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -78,6 +103,38 @@ export default {
   .tool {
     height: 60px;
     margin-left: 30px;
+  }
+  .total {
+    height: 60px;
+    height:30px ;
+  }
+  .item-ar {
+      margin: 10px 0;
+      padding: 10px 0;
+      border-bottom: 1px dashed #cccccc;
+    img {
+      width: 180px;
+      height: 120px;
+      margin-right: 20px;
+    }
+    .info {
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .tag {
+            width: 60px;
+        }
+        .date {
+            color: #999;
+            font-size: 14px;
+        }
+    }
+    .right {
+        span {
+            margin-left: 10px;
+        }
+    }
   }
 }
 </style>
