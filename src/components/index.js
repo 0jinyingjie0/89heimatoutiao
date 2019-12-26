17d01689e53edc25baa1c55a60a3eba1bca7aea7
jinyingjie <1406060330@qq.com>
2019-12-27 00:09:22 +0800

    封面组件实现

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import LayoutAside from './home/layout-aside'
 import LayoutHeader from './home/layout-header'
 import Bread from './common/bread'
 import { quillEditor } from 'vue-quill-editor'
-// import CoverImage from './publish/cover-image'
+import CoverImage from './publish/cover-image'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
@@ -13,6 +13,6 @@ export default {
     Vue.component('layout-header', LayoutHeader)
     Vue.component('bread-crumb', Bread)
     Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
-    // Vue.component('cover-image', CoverImage) // 注册一个封面组件
+    Vue.component('cover-image', CoverImage) // 注册一个封面组件
   }
 }
